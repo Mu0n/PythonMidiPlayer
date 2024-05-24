81937d885263960648126d74e35c142e2576d280
Mu0n <micjuneau@gmail.com>
2024-05-24 10:09:36 -0400

    Update midi_parser.py
    better structure, more printouts

--- midi_parser.py
+++ midi_parser.py
@@ -1,13 +1,10 @@
 import time
 import rtmidi
-
 from sys import argv
 
-#opening a port to MIDI out
-out = rtmidi.MidiOut()
-ports = out.get_ports()
-print(ports)
-out.open_port(1)
+# --- MIDI spec hard coded values ---
+# good reference is here: https://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html#BM1_1
+# from the midi.org itself: https://midi.org/summary-of-midi-1-0-messages
 
 MetaEndOfTrack     = 0x2F
 MetaSequence       = 0x00
@@ -26,9 +23,22 @@ MetaTimeSignature  = 0x58
 MetaKeySignature   = 0x59
 MetaSequencerSpecific = 0x7F
 
-    
-#parsing the .mid file passed as an argument upon execution
-def parse(file):
+
+# --- Opening a port to MIDI out ---
+# using the rtmidi package to fetch and open a MIDI out port.
+# note: you may need to use another port number than mine depending on your system
+# I expect Windows' typical port 0 is the GS Synth driver that's used to approximate midi sounds, 
+# used by Windows Media Player when playing .mid files
+
+
+
+# --- Parsing a midi file specified by the 1st argument upon execution
+
+def parse(portNum,file):
+    out = rtmidi.MidiOut()
+    ports = out.get_ports()
+    print(ports)
+    out.open_port(0)
     try:
         with open(file, 'rb') as ff:
         
@@ -53,6 +63,7 @@ def parse(file):
             i=21
             ff.seek(22)
             while i < data_buffer_len:
+                #print(i)
                 # time delta portion
                 finalValue = 0
                 nValue = 0
@@ -71,6 +82,7 @@ def parse(file):
                         finalValue = (nValue << 7) | (nByte & 0x7F)
                         if not nByte & 0x80:
                             break
+                    time.sleep(nValue/500)
                 
                 status_byte = ff.read(1)[0]
                 #print("eventByte ",status_byte)
@@ -78,11 +90,14 @@ def parse(file):
                 # MIDI Meta-event portion
                 if (status_byte & 0xFF) == 0xFF:
                     meta_byte = int.from_bytes(ff.read(1))
-                    print("Meta-Event",meta_byte)
+                    print("Status Byte",status_byte,"Meta-Event",meta_byte)
                     i+=1
                     if meta_byte == MetaSequence:
                         data1=ff.read(1)
                         data2=ff.read(1)
+                        #notemsg = [status_byte, meta_byte, data1, data2] 
+                        #print ("message is ",notemsg)
+                        #out.send_message(notemsg)
                         print("  Sequence Number: ", data1, data2)
                         i+=2
                     elif meta_byte == MetaText:
@@ -130,10 +145,14 @@ def parse(file):
                     elif meta_byte == MetaChannelPrefix:
                         data1=ff.read(1)
                         data2=ff.read(1)
+                        #notemsg = [status_byte, meta_byte, data1, data2] 
+                        #out.send_message(notemsg)
                         print("MIDI Channel Prefix: ", data1, data2)
                         i+=2	
                     elif meta_byte == MetaEndOfTrack:
                         data1=ff.read(1)
+                        #notemsg = [status_byte, meta_byte, data1] 
+                        #out.send_message(notemsg)
                         i+=1
                         print("*** END OF TRACK")
                     elif meta_byte == MetaSetTempo:
@@ -141,6 +160,8 @@ def parse(file):
                         data2=ff.read(1)[0]
                         data3=ff.read(1)[0]
                         data4=ff.read(1)[0]
+                        #notemsg = [status_byte, meta_byte, data1, data2,data3,data4] 
+                        #out.send_message(notemsg)
                         print("  Tempo: ", data1, data2, data3, data4)
                         i+=4	
                     elif meta_byte == MetaSMPTEOffset:
@@ -150,6 +171,9 @@ def parse(file):
                         data4=ff.read(1)[0]
                         data5=ff.read(1)[0]
                         data6=ff.read(1)[0]
+                        #notemsg = [0xF0, meta_byte, data1, data2,data3,data4,data5,data6] 
+                        #print ("SMTP message is ",notemsg)
+                        #out.send_message(notemsg)
                         print("  SMPTE Offset: ", data1, data2, data3, data4, data5, data6)
                         i+=6	
                     elif meta_byte == MetaTimeSignature:
@@ -158,12 +182,16 @@ def parse(file):
                         data3=ff.read(1)[0]
                         data4=ff.read(1)[0]
                         data5=ff.read(1)[0]
+                        #notemsg = [status_byte, meta_byte, data1, data2,data3,data4,data5] 
+                        #out.send_message(notemsg)
                         print("  Key Signature: ", data1, data2, data3, data4, data5)
                         i+=5	
                     elif meta_byte == MetaKeySignature:
                         data1=ff.read(1)[0]
                         data2=ff.read(1)[0]
                         data3=ff.read(1)[0]
+                        #notemsg = [status_byte, meta_byte, data1, data2,data3] 
+                        #out.send_message(notemsg)
                         print("  Key Signature: ", data1, data2, data3)
                         i+=3
                     elif meta_byte == MetaSequencerSpecific:
@@ -171,11 +199,15 @@ def parse(file):
                     else:
                         print("Unrecognised MetaEvent: ")
                         
+        # Program change   0xC_
+        # Channel Pressure 0xD_
                 elif 0xC0 <= status_byte <= 0xDF:
                     data1 = ff.read(1)[0]
                     notemsg = [status_byte,data1]
                     out.send_message(notemsg)
-                    i+=2
+                    i+=1
+        # Note off   0x8_
+        # Note on    0x9_
                 elif (0x80 <= status_byte <= 0x9F):
                     data1 = ff.read(1)[0]
                     data2 = ff.read(1)[0]
@@ -185,7 +217,10 @@ def parse(file):
                     print("note on ch ",status_byte & 0x0F, data1, data2)
                     out.send_message(notemsg)
                     i+=2
-                    time.sleep(2/(float(int.from_bytes(tdiv))))
+        # Polyphonic Key Pressure  0xA_ (Aftertouch)
+        # Control Change           0xB_
+        # (0xC_ and 0xD_ have been taken care of earlier already)
+        # Pitch Bend               0xE_
                 elif (0xA0 <= status_byte <= 0xBF) or (0xE0 <= status_byte <= 0xEF):
                     data1 = ff.read(1)[0]
                     data2 = ff.read(1)[0]
@@ -197,8 +232,8 @@ def parse(file):
     except Exception as e:
         print(f"Error reading the file: {e}")
     
+# --- Main entry point ---
 
-parse(argv[1])
-
+parse(argv[1],argv[2])
 
        
